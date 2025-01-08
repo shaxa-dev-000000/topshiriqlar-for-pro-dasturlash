3bf436aa81f3034adc62d5f49630ada67ddc5201
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-01-08 16:47:14 +0500

    Update topshiriq1.py

--- topshiriq1.py
+++ topshiriq1.py
@@ -4,11 +4,13 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== casefold ===> *2*
 # matn = 'Hello World'
 # result = matn.casefold()
 
 # print(f"natija: {result}")
+
 
 # # <=== center ===> *3*
 # matn = 'salom'
@@ -16,11 +18,13 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== count ===> *4*
 # matn = 'hello, my name is shakhriyor, my surname is ismatov'
 # x = matn.count('my')
 
 # print(f"natija: {x}")
+
 
 # # <=== encode ===> *5*
 # matn = 'hello wørld'
@@ -28,11 +32,13 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== endswith ===> *6*
 # matn = 'hello world'
 # result = matn.endswith('d')
 
 # print(f"natija: {result}")
+
 
 # # <=== find ===> *7*
 # matn = 'hello, my name is shakhriyor'
@@ -40,9 +46,11 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== format ===> *8*
 # matn = 'hello, I\'m {age:.2f} years old.'
 # print(matn.format(age = 17.86587))
+
 
 # # <=== index ===> *9*
 # matn = 'hello, my name is shakhriyor'
@@ -50,11 +58,13 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== isalnum ===> *10*
 # username = 'shahriyor000000'
 # x = username.isalnum()
 
 # print(f"natija: {x}")
+
 
 # # <=== islower ===> *11*
 # matn = 'hello world'
@@ -62,11 +72,13 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== isnumeric ===> *12*
 # matn = '1234'
 # result = matn.isnumeric()
 
 # print(f"natija: {result}")
+
 
 # # <=== isprintable ===> *13*
 # matn = 'hello, my name is shakhriyor'
@@ -74,11 +86,13 @@
 
 # print(f"natija: {result}")
 
+
 # # <=== isspace ===> *14*
 # matn = '  '
 # result = matn.isspace()
 
 # print(f"natija: {result}")
+
 
 # # <=== istitle ===> *15*
 # matn1 = 'Hello World'
@@ -88,11 +102,13 @@
 
 # print(f"natija1: {result1} \nnatija2: {result2}")
 
+
 # # <=== isupper ===> *16*
 # matn = 'HELLO WORLD'
 # result = matn.isupper()
 
 # print(f"natija: {result}")
+
 
 # # <=== join ===> *17*
 # sozlar = ['hello', 'world']
@@ -101,3 +117,69 @@
 
 # print(f"natija: {result}")
 
+
+# # <=== ljust ===> *18*
+# soz1 = 'hello'
+# soz2 = 'world'
+
+# x = soz1.ljust(10)
+# print(x, soz2)
+
+
+# # <=== lower ===> *19* 
+# matn = 'Hello World'
+# result = matn.lower()
+
+# print(f"natija: {result}")
+
+
+# # <=== lstrip ===> *20*
+# matn = ',,,,,ssssaaa....hello world'
+# result = matn.lstrip(',.sa')
+
+# print(f"natija: {result}")
+
+
+# # <=== maketrans ===> *21*
+# matn = 'hello world'
+# ozgartirish = str.maketrans('w', 'm')
+
+# print(f"natija: {matn.translate(ozgartirish)}")
+
+
+# # <=== partition ===> *22*
+# matn = 'hello, my name is Shakhriyor'
+# result = matn.partition('name')
+
+# print(f"natija: {result}")
+
+
+# # <=== replace ===> *23*
+# matn = 'I like apple'
+# result = matn.replace('apple', 'banana')
+
+# print(f"natija: {result}")
+
+
+# # <=== rfind ===> *24*
+# matn = 'hello world'
+# result = matn.rfind('world')
+
+# print(f"natija: {result}")
+
+
+# # <=== rindex ===> *25*
+# matn = 'I like apple, like banana'
+# result = matn.rindex('like')
+
+# print(f"natija: {result}")
+
+
+# # <=== rjust ===> *26*
+# matn = 'banana'
+# result = matn.rjust(20, 'a')
+
+# print(f"natija: {result}")
+
+
+# <===  ===>
