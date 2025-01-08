55d107620b34515526c557dffeb7d994e96d43d9
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-01-08 15:58:43 +0500

    Update topshiriq1.py

--- topshiriq1.py
+++ topshiriq1.py
@@ -55,3 +55,49 @@
 # x = username.isalnum()
 
 # print(f"natija: {x}")
+
+# # <=== islower ===> *11*
+# matn = 'hello world'
+# result = matn.islower()
+
+# print(f"natija: {result}")
+
+# # <=== isnumeric ===> *12*
+# matn = '1234'
+# result = matn.isnumeric()
+
+# print(f"natija: {result}")
+
+# # <=== isprintable ===> *13*
+# matn = 'hello, my name is shakhriyor'
+# result = matn.isprintable()
+
+# print(f"natija: {result}")
+
+# # <=== isspace ===> *14*
+# matn = '  '
+# result = matn.isspace()
+
+# print(f"natija: {result}")
+
+# # <=== istitle ===> *15*
+# matn1 = 'Hello World'
+# matn2 = 'HELOO WORLD'
+
+# result1, result2 = matn1.istitle(), matn2.istitle()
+
+# print(f"natija1: {result1} \nnatija2: {result2}")
+
+# # <=== isupper ===> *16*
+# matn = 'HELLO WORLD'
+# result = matn.isupper()
+
+# print(f"natija: {result}")
+
+# # <=== join ===> *17*
+# sozlar = ['hello', 'world']
+# belgi = ','
+# result = belgi.join(sozlar)
+
+# print(f"natija: {result}")
+
