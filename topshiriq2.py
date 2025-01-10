8707a3fea305dfe4e410c4fda78e26b1046fed6f
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-01-10 12:04:31 +0500

    Create topshiriq2.py

---
+++ topshiriq2.py
@@ -0,0 +1,33 @@
+# # <=== append() ===>
+# fruits = ['apple', 'banana', 'cherry']
+# fruit = 'peach'
+# fruits.append(fruit)
+# print(fruits) # output => ['apple', 'banana', 'cherry', 'peach']
+
+
+# # <=== clear() ===>
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.clear()
+# print(fruits) # output => []
+
+
+# # <=== copy() ===>
+# fruits1 = ['apple', 'banana', 'cherry']
+# fruits2 = fruits1.copy()
+# print(fruits2) # output => ['apple', 'banana', 'cherry']
+
+
+# # <=== count() ===>
+# fruits = ['apple', 'banana', 'cherry']
+# x = fruits.count('banana')
+# print(x) # output => 1
+
+
+# # <=== extend() ===>
+# fruits = ['apple', 'banana', 'cherry']
+# vegetables = ['potato', 'tomato', 'onion']
+# fruits.extend(vegetables)
+# print(fruits) # output => ['apple', 'banana', 'cherry', 'potato', 'tomato', 'onion']
+
+
+# 
