31582de9408128d531acb1f3e936fb064ea384a3
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-02-14 21:50:25 +0500

    .

--- class1.py
+++ class1.py
@@ -6,3 +6,16 @@
 #         print(self.a)
 #
 # Oson1.output_a(Oson1)
+
+
+# # <== 2 - masala ==>
+# class Oson2:
+#     a = 3
+#     b = 4
+#
+#     def summa(self):
+#         print(self.a + self.b)
+#
+# Oson2.summa(Oson2)
+
+
