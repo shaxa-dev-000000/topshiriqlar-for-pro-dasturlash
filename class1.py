549af081a4ba758a83183f285bdea4a17d439868
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-02-14 21:57:30 +0500

    .

--- class1.py
+++ class1.py
@@ -19,3 +19,57 @@
 # Oson2.summa(Oson2)
 
 
+# # <=== 3 - masala ===>
+# class Oson3:
+#     a = 10
+#
+#     def plus_minus(self):
+#         if self.a > 0:
+#             print("Musbat")
+#         elif self.a == 0:
+#             print("a 0ga teng")
+#         else:
+#             print("Manfiy")
+#
+# Oson3.plus_minus(Oson3)
+
+
+# # <=== 4 - masala ===>
+# class Oson4:
+#     a = 5
+#
+#     def odd_even(self):
+#         if self.a % 2 == 0:
+#             print("Juft")
+#         else:
+#             print("Toq")
+#
+# Oson4.odd_even(Oson4)
+
+
+# # <=== 5 - masala ===>
+# class Oson5:
+#     a = 2
+#     b = 3
+#
+#     def daraja(self):
+#         print(self.a ** self.b)
+#
+# Oson5.daraja(Oson5)
+
+
+# # <=== 6 - masala ===>
+# class MyClass6:
+#     words = []
+#
+#     def add_word(self, word):
+#         self.words.append(word)
+#
+#     def remove(self, word):
+#         if word in self.words:
+#             self.words.remove(word)
+#         else:
+#             print("Bunday so'z mavjud emas")
+#
+# MyClass6.add_word(MyClass6, "hello")
+# MyClass6.add_word(MyClass6, "world")
